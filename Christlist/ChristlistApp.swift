2c2ac08634a0d83d6f3d123a7f68d36896f6ba76
Fahim <fahimhussain21@gmail.com>
2021-12-24 10:52:38 -0500

    Initial Commit

---
+++ Christlist/ChristlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChristlistApp.swift
+//  Christlist
+//
+//  Created by Fahim Hussain on 12/24/21.
+//
+
+import SwiftUI
+
+@main
+struct ChristlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
